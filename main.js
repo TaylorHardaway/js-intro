ab16d486bbbebdd95642198fb1f0c94311c6f6ad
TaylorHardaway <gthardaway@gmail.com>
2021-01-16 11:08:04 -0600

    variables, data types, Math

--- main.js
+++ main.js
@@ -1,1 +1,13 @@
-console.log("hello I am a main.js file")
+//console.log("hello I am a main.js file")
+
+let firstName = `Beyonce`;
+let lastName = `Knowles`;
+
+console.log(firstName + lastName)
+console.log(`First Name: ${firstName} Last Name: ${lastName}`)
+
+firstName = 'Taylor'
+lastName = 'Hardaway'
+
+console.log(firstName + lastName)
+console.log(`First Name: ${firstName} Last Name: ${lastName}`)
