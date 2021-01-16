d4b4727a945c8b653dabe225a6b5cbb852b007b5
TaylorHardaway <gthardaway@gmail.com>
2021-01-16 13:09:34 -0600

    learning more logic

--- main.js
+++ main.js
@@ -1,1 +1,35 @@
 console.log("hello I am a main.js file")
+
+
+//function thisIsTrue(arg1, arg2) {
+//if (arg !== 'nice' && arg !== 'Nice') {
+//  console.log('you are ${arg1}');
+//} else {
+//  console.log('false');//
+//}
+//}
+
+//thisIsTrue('nice', 'Nice');
+
+
+function thisIsTrue(arg1, arg2) {
+  if (arg1.length < 3) {
+    console.log('you are ${arg1}');
+  } else if ('you are ${arg2}')
+  } 
+  else {
+    console.log('you are nothing');
+  }
+  }
+  
+  thisIsTrue('nice', 'Mickey Mouse');
+
+  const canDrive = (age) => {
+    if (age >= 15) {
+      return true;
+    }
+
+    return false;
+  }
+
+  console.log(canDrive(13));
