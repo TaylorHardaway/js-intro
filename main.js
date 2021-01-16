64c6739bf41708007f181dd1ecf009771ecf1629
TaylorHardaway <gthardaway@gmail.com>
2021-01-16 11:16:20 -0600

    added saved data

--- main.js
+++ main.js
@@ -1,5 +1,7 @@
 //console.log("hello I am a main.js file")
 
+
+// *********START VARIABLES*********//
 let firstName = `Beyonce`;
 let lastName = `Knowles`;
 
@@ -9,5 +11,45 @@ console.log(`First Name: ${firstName} Last Name: ${lastName}`)
 firstName = 'Taylor'
 lastName = 'Hardaway'
 
-console.log(firstName + lastName)
-console.log(`First Name: ${firstName} Last Name: ${lastName}`)
+// console.log(firstName + lastName)
+// console.log(`First Name: ${firstName} Last Name: ${lastName}`)
+
+//const favoriteFood = 'Tacos'//
+//console.log(favoriteFood)
+
+//favoriteFood = 'Gumbo'//
+// console.log("favoriteFood")
+
+//********END VARIABLES***********//
+
+//********START DATA TYPES***********//
+
+//const dog ='bark'
+//console.log(typeof dog)
+
+//const answer = 33;
+//const negative = -14;
+
+//console.log(typeof answer, typeof negative)
+
+//const iLoveJavascript = true;
+//console.log(typeof iLoveJavascript);
+
+//********END DATA TYPES***********//
+
+//********START MATH***********//
+
+//console.log(Math.pow(2, 2));
+//console.log(Math.pow(3, 2));
+//console.log(Math.pow(3, 3));
+
+//console.log(Math.round(6.5));
+//console.log(Math.round(6.45));
+
+//console.log(Math.floor(6.99999));
+//console.log(Math.ceil(6.0000001));
+
+const cat ='Kitty';
+console.log(cat.length); // 5
+
+//********END MATH***********//
