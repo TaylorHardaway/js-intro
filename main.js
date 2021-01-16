60861b94247782c0ac016d5ee8cfef5153387e5d
TaylorHardaway <gthardaway@gmail.com>
2021-01-16 12:36:56 -0600

    saved everything

--- main.js
+++ main.js
@@ -1,1 +1,31 @@
-console.log("hello I am a main.js file")
+//console.log("hello I am a main.js file")
+
+//function sayHello() {
+  //console.log("Hello!");
+//}
+
+//const sayHello = function() {
+//  console.log("Hello!")
+//}
+//const sayHello = ()=> {
+  //console.log("Hello!")
+//}
+//sayHello();
+
+//************FUCTIONS WITH ARGUEMENTS**************//
+
+//function logsHello(name) {
+  //console.log('Hello, ${name}')
+
+//}
+
+//logsHello('Taylor');
+
+const addTwoNumbers = (num1, num2) => {
+      const sum = num1 + num2;
+      console.log(sum);
+      return sum;
+
+}
+
+console.log(addTwoNumbers(5, 7));
